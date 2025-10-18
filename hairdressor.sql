ff820a42fe98620ce2e14f73b0de2ad7e839bf1c
MiaLouw-MM <u23524465@tuks.co.za>
2025-10-18 10:19:56 +0200

    Hairdressor Database created

---
+++ hairdressor.sql
@@ -0,0 +1,16 @@
+create table hairdressor(
+hairdressor_id INTEGER primary key AUTOINCREMENT,
+hairdressor_name varchar(100) not null,
+hairdressor_surname varchar(100) not null,
+hairdressor_phone varchar(15),
+hairdressor_email varchar(100) unique
+);
+
+INSERT INTO hairdressor (hairdressor_name, hairdressor_surname, hairdressor_phone, hairdressor_email)
+VALUES 
+("Caty", "Brown", "0798888888", "catyBrown@hairdressor.com"),
+("Jake", "White", "0797777777", "jakeWhite@hairdressor.com"),
+("Stacey", "Green", "0796666666", "staceygreen@hairdressor.com"),
+("Josh", "King", "0795555555", "joshB@hairdressor.com");
+
+SELECT * FROM hairdressor;
